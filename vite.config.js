9527071ba885e4070c795d8ff2b07d9efd7070b8
KarakterKombat <mrtaabooodi81@gmail.com>
2024-08-24 17:52:34 +0330

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -14,11 +14,5 @@ export default defineConfig({
         hmr: {
             clientPort: 443, // Run the websocket server on the SSL port
         },
-        rewrites: [
-            {
-                source: "/(.*)",
-                destination: "/",
-            },
-        ],
     },
 });
